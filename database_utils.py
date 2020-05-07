b7a9e1beca8c6e29a40dfa8cda0f6483b5d0fec6
Logan Haug <loganhaug@Logans-MacBook-Pro.local>
2020-05-07 15:21:14 -0700

    Refactor parameter name "document" in add_document() in database_utils.py

--- database_utils.py
+++ database_utils.py
@@ -6,22 +6,22 @@ import pymongo
 
 DB = pymongo.MongoClient('localhost', 27017).inspire_bot_db
 
-def add_document(collection: str, data: dict) -> None:
+def add_document(collection: str, document: dict) -> None:
     """Adds a document to the specified collection
     'collection' is the collection name to insert the document into
-    'data' is a dictionary containing the data to add to the collection
+    'document' is a dictionary containing the data to add to the collection
     returns nothing"""
     if not isinstance(collection, str):
         raise TypeError('Parameter "collection" must be a string')
-    if not isinstance(data, dict):
-        raise TypeError('Parameter "data" must be a dictionary')
-    DB[collection].insert_one(data)
+    if not isinstance(document, dict):
+        raise TypeError('Parameter "document" must be a dictionary')
+    DB[collection].insert_one(document)
 
 
-def find_random_document(collection) -> dict:
+def find_random_document(collection: str) -> dict:
     """Finds a random document from the user-defined collection
     returns the document"""
-    document = list(DB[collection].aggregate([{'$sample': {'size': 1}}]))[0]
-    if not isinstance(document, dict):
-        return {'text': 'There are no documents stored, try storing some : )'}
-    return document
+    document = list(DB[collection].aggregate([{'$sample': {'size': 1}}]))
+    if document != []:
+        return document[0]
+    return {'text': 'There are no documents stored, try storing some : )'}
