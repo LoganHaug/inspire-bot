13a261d07fbac7bf8f832521211e2f069b5493bc
Logan Haug <loganhaug@Logans-MacBook-Pro.local>
2020-05-06 20:10:54 -0700

    Add on_ready event handler in inspire_bot.py

--- inspire_bot.py
+++ inspire_bot.py
@@ -7,5 +7,8 @@ with open('token.txt', 'r') as token_file:
     TOKEN = token_file.read()
 # Starts the client
 INSPIRE_BOT = discord.Client()
+@INSPIRE_BOT.event
+async def on_ready():
+    print(f'{INSPIRE_BOT.user} connected to discord : )')
 # Runs the client
 INSPIRE_BOT.run(TOKEN)
