8a846a35b53545c480bdda2e709f0042a13fab89
Logan Haug <loganhaug@Logans-MacBook-Pro.local>
2020-05-06 20:09:19 -0700

    Add some comments in inspire_bot.py

--- inspire_bot.py
+++ inspire_bot.py
@@ -2,9 +2,10 @@
 # External Imports
 import discord
 # No Internal Imports
-
+# Grabs the bot token
 with open('token.txt', 'r') as token_file:
     TOKEN = token_file.read()
-
+# Starts the client
 INSPIRE_BOT = discord.Client()
+# Runs the client
 INSPIRE_BOT.run(TOKEN)
