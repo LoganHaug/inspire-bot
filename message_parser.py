87ef0f6c0df0a7c09a3aabd0b083e39876f9de7d
Logan Haug <logannhaug@gmail.com>
2020-05-15 13:53:43 -0700

    Add basic functionality in message_parser.py

--- message_parser.py
+++ message_parser.py
@@ -1,7 +1,7 @@
 """Message Parser
 Parses discord messages and executes commands based on message text"""
 # External Imports
-import re
+import time
 import shlex
 import yaml
 # Internal Imports
@@ -21,14 +21,70 @@ for command in COMMAND_SCHEMA['commands']:
         COMMANDLIST[alias] = command
 
 
-def parse_message(message) -> None:
+def has_privledges(user_id: int) -> int:
+    """Checks if a user is an admin or moderator
+    'user_id' is the discord user id number of the user to check
+    returns 2 if user is admin, 1 if user is moderator, and 0 otherwise"""
+    user_document = database_utils.find_document('users', query={'user-id': user_id})
+    if user_document is None:
+        return 0
+    if user_document['is-admin'] is True:
+        return 2
+    if user_document['is-moderator'] is True:
+        return 1
+    return 0
+
+def add_quote(message_text: list, user_id: int) -> str:
+    """Adds a quote to the quotes collection
+    'message_text' is a list of strings that represent the message text split
+    by spaces
+    'user_id' is the discord user id number of the user who issued the command
+    returns message text the bot should send to the issued command channel"""
+    if has_privledges(user_id) in [1, 2]:
+        if len(message_text) < 3:
+            return COMMAND_SCHEMA['bot-messages']['failed-quote']
+        # list of message_text: command call, quote text, quote author
+        document = {'quote-text': message_text[1],
+                    'quote-author': message_text[2],
+                    'time-quoted': time.time(), 'inserted_by': user_id}
+        if database_utils.find_document('quotes', {'quote-text': message_text[1]}) is not None:
+            return COMMAND_SCHEMA['bot-messages']['already-quoted']
+        else:
+            database_utils.add_document('quotes', document)
+        return COMMAND_SCHEMA['bot-messages']['successful-quote-insert']
+    return COMMAND_SCHEMA['bot-messages']['no-privledges']
+
+def parse_message(message_text: str, user_id: int) -> str:
     """Parses a user message, and executes commands based on message text
-    'message' is the message object of the user's message to parse"""
-    if not isinstance(message, str):
-        raise TypeError('argument "message" is not a string')
-    if not message.startswith(COMMAND_SCHEMA['command-prefix']):
+    'message_text' is the message text to parse
+    'author_id' is the discord id of the user that sent the message
+    returns a message for the bot to say in response to a command"""
+    database_utils.validate_arguments({'message_text': [message_text, str],
+                                       'user-id': [user_id, int]})
+    if not message_text.startswith(COMMAND_SCHEMA['command-prefix']):
         return None
-    message = shlex.split(message)
-    message[0] = message[0][1:-1]
-    if message[0] in COMMMAND_LIST:
-        pass
+    message_text = shlex.split(message_text)
+    message_text[0] = message_text[0][1:]
+    if message_text[0] in COMMANDLIST:
+        user_document = database_utils.find_document('users', query={'user-id': user_id})
+        if user_document is None:
+            user_document = {'user-id': user_id,
+                             'is-admin': False,
+                             'is-moderator': False,
+                             'num-commands': 1}
+            database_utils.add_document('users', user_document)
+        else:
+
+            database_utils.update_document('users', user_document,
+                                           {'num-commands': user_document['num-commands'] + 1})
+        user_command = COMMANDLIST[message_text[0]]
+        function = user_command + '('
+        num_arguments = 0
+        for argument in COMMANDS[user_command]['parameters']:
+            if num_arguments != len(COMMANDS[user_command]['parameters']) - 1:
+                function += argument + ', '
+            else:
+                function += argument + ')'
+            num_arguments += 1
+        return eval(function)
+    return COMMAND_SCHEMA['bot-messages']['no-command']
