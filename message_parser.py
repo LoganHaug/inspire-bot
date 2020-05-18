a8b263c80de5ae6ef3cd4675e647bcbfd1535870
Logan Haug <logannhaug@gmail.com>
2020-05-17 18:14:33 -0700

    Fix stuff lol

--- message_parser.py
+++ message_parser.py
@@ -20,23 +20,8 @@ for command in COMMAND_SCHEMA['commands']:
     for alias in COMMANDS[command]['alias']:
         COMMANDLIST[alias] = command
 
-def add_moderator(user_id: int) -> str:
-    """Gives moderator privledges to a user
-    returns a message for the bot to say"""
-    user_document = database_utils.find_document('users', query={'user-id': user_id})
-    database_utils.update_document('users', user_document, {'is-moderator': True})
-    return COMMAND_SCHEMA['bot-messages']['made-user-mod']
 
-
-def remove_moderator(user_id: int) -> str:
-    """Revokes moderator privledges from a user
-    returns a message for the bot to say"""
-    user_document = database_utils.find_document('users', query={'user-id': user_id})
-    database_utils.update_document('users', user_document, {'is-moderator': False})
-    return COMMAND_SCHEMA['bot-messages']['removed-user-mod']
-
-
-def has_privledges(user_id: int) -> int:
+def has_priviledges(user_id: int) -> int:
     """Checks if a user is an admin or moderator
     'user_id' is the discord user id number of the user to check
     returns 2 if user is admin, 1 if user is moderator, and 0 otherwise"""
@@ -50,24 +35,97 @@ def has_privledges(user_id: int) -> int:
     return 0
 
 
+def add_user(user_id, is_admin, is_moderator) -> None:
+    """Adds the user to the users collection
+    'user_id' is the id of the user to add to the users collection
+    returns nothing"""
+    # Validate the arguments
+    database_utils.validate_arguments({'user_id': [user_id, int],
+                                       'is_admin': [is_admin, bool],
+                                       'is_moderator': [is_moderator, bool]})
+    # Assemble a new user document
+    user_document = {'user-id': user_id,
+                     'is-admin': is_admin,
+                     'is-moderator': is_moderator,
+                     'num-commands': 0}
+    # Add the document to the user table
+    database_utils.add_document('users', user_document)
+
+def add_moderator(user_id: int, mentions: list) -> str:
+    """Gives moderator priviledges to a user
+    message_text is the message text split using shlex along spaces
+    returns a message for the bot to say"""
+    if has_priviledges(user_id) not in [2]:
+        return COMMAND_SCHEMA['bot-messages']['no-priviledges']
+    if mentions == []:
+        return COMMAND_SCHEMA['bot-messages']['make-mod-fail']
+    # Try to execute this code
+    try:
+        # Iterate over the mentions in the message
+        for mention in mentions:
+            # Obtain the user document
+            user_document = database_utils.find_document('users', query={'user-id': mention.id})
+            # If no user document was found add it and make the user a moderator
+            if user_document is None:
+                add_user(mention.id, False, True)
+            # Otherwise update the users moderator status
+            else:
+                database_utils.update_document('users', user_document, {'is-moderator': True})
+        return COMMAND_SCHEMA['bot-messages']['made-user-mod']
+    # If it fails, tell the user it failed
+    except:
+        return COMMAND_SCHEMA['bot-messages']['make-mod-fail']
+
+
+def remove_moderator(user_id: int, mentions: list) -> str:
+    """Revokes moderator priviledges from a user
+    'mentions' is the list of user objects that were mention in the message
+    returns a message for the bot to say"""
+    if has_priviledges(user_id) not in [2]:
+        return COMMAND_SCHEMA['bot-messages']['no-priviledges']
+    if mentions == []:
+        return COMMAND_SCHEMA['bot-messages']['remove-mod-fail']
+    # Try to execute this code
+    try:
+        # Iterate over the mentions in the message
+        for mention in mentions:
+            # Find the user document
+            user_document = database_utils.find_document('users', query={'user-id': mention.id})
+            # If the user document cannot be found add it
+            if user_document is None:
+                add_user(mention.id, False, False)
+            # Otherwise remove the moderator status from the user
+            else:
+                database_utils.update_document('users', user_document, {'is-moderator': False})
+        # Tell the user it removed the moderator status
+        return COMMAND_SCHEMA['bot-messages']['removed-user-mod']
+    # If the code fails, tell the user it failed
+    except:
+        return COMMAND_SCHEMA['bot-messages']['remove-mod-fail']
+
+
 def add_quote(message_text: list, user_id: int) -> str:
     """Adds a quote to the quotes collection
     'message_text' is a list of strings that represent the message text split
     by spaces
     'user_id' is the discord user id number of the user who issued the command
     returns message text the bot should send to the issued command channel"""
-    if has_privledges(user_id) in [1, 2]:
+    # Checks if the user is an admin or moderator
+    if has_priviledges(user_id) in [1, 2]:
+        # If the length of message text is less than 3, arguments are missing
         if len(message_text) < 3:
             return COMMAND_SCHEMA['bot-messages']['failed-quote']
         # list of message_text: command call, quote text, quote author
         document = {'quote-text': message_text[1],
                     'quote-author': message_text[2],
                     'time-quoted': time.time(), 'inserted_by': user_id}
+        # If the quote already exists, tell the user its already quoted
         if database_utils.find_document('quotes', {'quote-text': message_text[1]}) is not None:
             return COMMAND_SCHEMA['bot-messages']['already-quoted']
+        # Add the qutoe document
         database_utils.add_document('quotes', document)
         return COMMAND_SCHEMA['bot-messages']['successful-quote-insert']
-    return COMMAND_SCHEMA['bot-messages']['no-privledges']
+    return COMMAND_SCHEMA['bot-messages']['no-priviledges']
 
 
 def find_random_quote() -> str:
@@ -76,40 +134,53 @@ def find_random_quote() -> str:
     return f'```"{document["quote-text"]}"\n   -{document["quote-author"]}```'
 
 
-def parse_message(message_text: str, user_id: int) -> str:
+def parse_message(message_text: str, user_id: int, mentions: list) -> str:
     """Parses a user message, and executes commands based on message text
     'message_text' is the message text to parse
     'author_id' is the discord id of the user that sent the message
+    'mentions' is the list of member objects that were mentioned in the message
     returns a message for the bot to say in response to a command"""
+    # Validates that the arguments are the correct type
     database_utils.validate_arguments({'message_text': [message_text, str],
-                                       'user-id': [user_id, int]})
+                                       'user-id': [user_id, int],
+                                       'mentions': [mentions, list]})
+    # If the message doesn't start with the start character, do nothing
     if not message_text.startswith(COMMAND_SCHEMA['command-prefix']):
         return None
+    # Split the message text along spaces, but not within quotations
     message_text = shlex.split(message_text)
+    # Remove the command prefix from the message text
     message_text[0] = message_text[0][1:]
+    # If the command is a valid command
     if message_text[0] in COMMANDLIST:
+        # Find the user document
         user_document = database_utils.find_document('users', query={'user-id': user_id})
+        # Add the user to the users collection if they aren't in there
         if user_document is None:
-            user_document = {'user-id': user_id,
-                             'is-admin': False,
-                             'is-moderator': False,
-                             'num-commands': 1}
-            database_utils.add_document('users', user_document)
+            add_user(user_id, False, False)
+        # Otherwise increment their number of commands by 1
         else:
-
             database_utils.update_document('users', user_document,
                                            {'num-commands': user_document['num-commands'] + 1})
+        # This line accounts for alias' of the commands
         user_command = COMMANDLIST[message_text[0]]
+        # Start making the command into code we can execute
         function = user_command + '('
+        # If the command the user wants to run has no parameters, run it
         if COMMANDS[user_command]['parameters'] is None:
             function += ')'
             return eval(function)
         num_arguments = 0
+        # Iterate through all of the parameters of the given command
         for argument in COMMANDS[user_command]['parameters']:
+            # If the current parameter isn't the last one, use commmas
             if num_arguments != len(COMMANDS[user_command]['parameters']) - 1:
                 function += argument + ', '
+            # Otherwise close up the function call
             else:
                 function += argument + ')'
             num_arguments += 1
+        # Run the function
         return eval(function)
+    # If the command doesn't execute, tell the user it didn't work
     return COMMAND_SCHEMA['bot-messages']['no-command']
